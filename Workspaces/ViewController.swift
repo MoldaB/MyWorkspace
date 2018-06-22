94a16afe3d3f4d5f8460b592d9892bbfafe91a49
Molda Beta <molda_beta@Moldas-MacBook-Pro.local>
2018-06-22 20:05:15 +0300

    Initial commit

---
+++ Workspaces/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Workspaces
+//
+//  Created by Molda on 21/06/2018.
+//  Copyright © 2018 Molda. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
